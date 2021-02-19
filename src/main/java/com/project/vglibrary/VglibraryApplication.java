7fc8ac9cd62cee9e9891dd80abff231d8372035b
Jayden-Chiu <jaydenchiu@rogers.com>
2021-02-19 03:32:10 -0500

    init commit

---
+++ src/main/java/com/project/vglibrary/VglibraryApplication.java
@@ -0,0 +1,13 @@
+package com.project.vglibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VglibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VglibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/vglibrary/VglibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VglibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
