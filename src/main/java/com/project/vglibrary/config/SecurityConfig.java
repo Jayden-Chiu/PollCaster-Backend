3cc1886090d93d2c4f192cf4a5f700f9fb6647c5
Jayden-Chiu <jaydenchiu@rogers.com>
2021-02-25 01:37:53 -0500

    added AuthenticationManager bean to SecurityConfig

--- src/main/java/com/project/vglibrary/config/SecurityConfig.java
+++ src/main/java/com/project/vglibrary/config/SecurityConfig.java
@@ -1,14 +1,18 @@
-package com.project.vglibrary.security;
+package com.project.vglibrary.config;
 
+import com.project.vglibrary.security.jwt.JwtAuthenticationFilter;
 import com.project.vglibrary.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,14 +26,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       auth.userDetailsService(userDetailsService);
     }
 
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
