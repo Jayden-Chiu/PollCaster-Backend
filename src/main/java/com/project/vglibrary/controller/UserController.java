f23b79143cd1cc70e8c0faadf01289802b986061
Jayden-Chiu <jaydenchiu@rogers.com>
2021-02-26 23:12:09 -0500

    switch to UserProfile payload responses in user api

--- src/main/java/com/project/vglibrary/controller/UserController.java
+++ src/main/java/com/project/vglibrary/controller/UserController.java
@@ -2,6 +2,7 @@ package com.project.vglibrary.controller;
 
 import com.project.vglibrary.entity.User;
 import com.project.vglibrary.exception.ResourceNotFoundException;
+import com.project.vglibrary.payload.response.UserProfile;
 import com.project.vglibrary.repository.UserRepository;
 import com.project.vglibrary.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,26 +19,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // Get all users
-    @GetMapping("/")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    // Get user by id
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable(value = "id") Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
-    }
-
-    // Delete user by id
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id",
-                id));
-
-        userRepository.delete(user);
-
-        return ResponseEntity.ok().build();
+    public UserProfile getUserById(@PathVariable(value = "id") Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+        return new UserProfile(user);
     }
 }
---
+++ src/main/java/com/project/vglibrary/payload/response/UserProfile.java
@@ -0,0 +1,29 @@
+package com.project.vglibrary.payload.response;
+
+import com.project.vglibrary.entity.User;
+
+public class UserProfile {
+    private Long id;
+    private String username;
+
+    public UserProfile(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
