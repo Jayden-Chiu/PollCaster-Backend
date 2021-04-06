0af2c7227dc7ea2a38bdacbd62edeac2de5d2619
Jayden-Chiu <jaydenchiu@rogers.com>
2021-04-06 19:31:20 -0400

    added repository annotation to VoteRepository

--- src/main/java/com/project/pollcaster/repository/VoteRepository.java
+++ src/main/java/com/project/pollcaster/repository/VoteRepository.java
@@ -2,9 +2,11 @@ package com.project.pollcaster.repository;
 
 import com.project.pollcaster.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     Optional<Vote> findById(Long id);
 }
