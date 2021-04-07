087672f4501d98908a1db0578d986bd510a53e21
Jayden-Chiu <jaydenchiu@rogers.com>
2021-04-07 05:32:45 -0400

    add custom sql query to get poll by user and poll id

--- src/main/java/com/project/pollcaster/repository/VoteRepository.java
+++ src/main/java/com/project/pollcaster/repository/VoteRepository.java
@@ -2,6 +2,8 @@ package com.project.pollcaster.repository;
 
 import com.project.pollcaster.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     Optional<Vote> findById(Long id);
+
+    @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id = :pollId")
+    Optional<Vote> findByUserIdAndPollId(@Param("userId") Long userId, @Param("pollId") Long pollId);
 }
